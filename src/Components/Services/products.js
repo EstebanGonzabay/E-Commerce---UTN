5ab3f1dc88e3ee1c023f8df2e2b2bf1839048bbe
Esteban Gonzabay <Gonzabay52@gmail.com>
2025-06-19 17:38:33 -0500

    actualizacion

--- src/Components/Services/products.js
+++ src/Components/Services/products.js
@@ -1,18 +1,15 @@
 export const getProducts = async() =>{
-    try{const response = await fetch(
-        'http://localhost:5173/api/products.json',
-        {
-            method: 'GET'
-        }
-    )
 
-    const data = await response.json()
-    return data
-}
-catch(error){
-    console.error('Error al obtener productos: ', error)
-    return null
-}
+  try {
+    const response = await fetch('/api/products.json');
+    if (!response.ok) {
+      throw new Error('Error al obtener los productos');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
 }
 
 export const getProductById = async () =>{
